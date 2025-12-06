39c07858c42add1b94955ba80d26904050f97d71
Revanth2374 <revanth2374nre@gmail.com>
2025-12-06 12:57:36 +0530

    adding files

---
+++ PracticeSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSwiftApp.swift
+//  PracticeSwift
+//
+//  Created by Revanth Narne on 06/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
